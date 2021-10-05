5c4e065ac224820ccb9f93df958f79b9f24387e3
Vorobishek <vorobishek@list.ru>
2021-10-05 00:43:35 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 56800;
+        int milePrice = 20;
+        int mileAdd;
+
+        mileAdd = ticketPrice / milePrice;
+
+        System.out.println("Количество бонусных миль = " + mileAdd);
+
+    }
+}
